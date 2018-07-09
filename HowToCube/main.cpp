e8f801bf45c503d0fd3f7a22eb4996b5ea362b04
Jack Zeligson <jackzeligson@jacks-macbook-pro.local>
2018-07-09 15:50:36 -0400

    Initial Commit

---
+++ HowToCube/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HowToCube
+//
+//  Created by Jack Zeligson on 7/9/18.
+//  Copyright © 2018 Jack Zeligson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
